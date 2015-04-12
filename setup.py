37fee7f86dd86cc2c6cbe45bd24dbf331b8fd6df
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-04-12 21:13:04 +0000

    initial checkin
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti./nti.zope_catalog/trunk@62960 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ setup.py
@@ -0,0 +1,71 @@
+import codecs
+from setuptools import setup, find_packages
+
+VERSION = '0.0.0'
+
+entry_points = {
+	'console_scripts': [
+	],
+}
+
+TESTS_REQUIRE = [
+	'fudge',
+	'nose',
+	'nose-timer',
+	'nose-pudb',
+	'nose-progressive',
+	'nose2[coverage_plugin]',
+	'pyhamcrest',
+	'zope.testing',
+	'nti.nose_traceback_info'
+]
+
+setup(
+	name='nti.zope_catalog',
+	version=VERSION,
+	author='Jason Madden',
+	author_email='jason@nextthought.com',
+	description="NTI ZODB",
+	long_description=codecs.open('README.rst', encoding='utf-8').read(),
+	license='Proprietary',
+	keywords='ZODB Catalog',
+	classifiers=[
+		'Intended Audience :: Developers',
+		'Natural Language :: English',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 2',
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: Implementation :: CPython'
+        'Programming Language :: Python :: Implementation :: PyPy'
+	],
+	packages=find_packages('src'),
+	package_dir={'': 'src'},
+	namespace_packages=['nti'],
+	tests_require=TESTS_REQUIRE,
+	install_requires=[
+		'setuptools',
+        'BTrees',
+        'persistent',
+        'pytz',
+        'six',
+        'zc.catalog',
+		'ZODB',
+        'zope.catalog',
+		'zope.component',
+        'zope.container',
+        'zope.index',
+		'zope.interface',
+		'zope.security',
+		'nti.common'
+	],
+	extras_require={
+		'test': TESTS_REQUIRE,
+	},
+	dependency_links=[
+		'git+https://github.com/NextThought/nti.nose_traceback_info.git#egg=nti.nose_traceback_info'
+	],
+	entry_points=entry_points
+)
