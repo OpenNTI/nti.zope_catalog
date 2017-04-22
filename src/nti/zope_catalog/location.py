bf61ea06323180f6d56fe516722dfd6bd39ae760
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-04-22 03:48:18 +0000

    copy from pyramid
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.zope_catalog/trunk@111185 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/zope_catalog/location.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope.interface.interfaces import IInterface
+
+
+def lineage(resource):
+    while resource is not None:
+        yield resource
+        try:
+            resource = resource.__parent__
+        except AttributeError:
+            resource = None
+
+
+def find_interface(resource, class_or_interface):
+    if IInterface.providedBy(class_or_interface):
+        test = class_or_interface.providedBy
+    else:
+        def test(arg): 
+            return isinstance(arg, class_or_interface)
+    for location in lineage(resource):
+        if test(location):
+            return location
