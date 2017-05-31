f284a702dc25e1b998aac76ff69d6387d7ae335e
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-05-31 01:55:07 +0000

    should run on python36
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.zope_catalog/trunk@113811 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -32,8 +32,10 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: Implementation :: CPython'
-        'Programming Language :: Python :: Implementation :: PyPy'
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
     ],
     zip_safe=True,
     packages=find_packages('src'),
